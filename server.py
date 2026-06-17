c89996b091e1f869f12a738d5ffba783a7ca8dc0
t <t@t.co>
2026-06-17 18:31:42 -0500

    dump openhost env

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-import json
+import json, os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 class H(BaseHTTPRequestHandler):
     def _h(self):
-        body = json.dumps({k: v for k, v in self.headers.items()}, indent=2).encode()
+        out = {
+            "headers": {k: v for k, v in self.headers.items()},
+            "openhost_env": {k: v for k, v in os.environ.items() if k.startswith("OPENHOST")},
+        }
+        body = json.dumps(out, indent=2).encode()
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(body)))
