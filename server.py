be01aec8eaa4836b28417f42d92b6062aad08f63
t <t@t.co>
2026-06-17 17:46:39 -0500

    header echo

---
+++ server.py
@@ -0,0 +1,14 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+class H(BaseHTTPRequestHandler):
+    def _h(self):
+        body = json.dumps({k: v for k, v in self.headers.items()}, indent=2).encode()
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)
+    do_GET = _h
+    do_POST = _h
+    def log_message(self, *a): pass
+HTTPServer(("0.0.0.0", 8080), H).serve_forever()
